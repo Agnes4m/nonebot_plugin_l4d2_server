765b5ebe8bf6e9dda3673f540341aae93fd75db0
afu <735803792@qq.com>
2023-02-18 02:50:03 +0800

    🐛 修复了linux服务器下rar和7z的错误

--- nonebot_plugin_l4d2_server/l4d2_file/__init__.py
+++ nonebot_plugin_l4d2_server/l4d2_file/__init__.py
@@ -4,18 +4,20 @@ from time import sleep
 import sys
 import os
 from ..utils import get_file,get_vpk
-from ..config import map_path
+from ..config import systems
 from nonebot.log import logger
 from py7zr import SevenZipFile
 from rarfile import RarFile
+import rarfile
+from pyunpack import Archive
 
 
 
-async def updown_l4d2_vpk(map_path,name,url):
+async def updown_l4d2_vpk(map_paths,name,url):
     """从url下载压缩包并解压到位置"""
     original_vpk_files = []
-    original_vpk_files = get_vpk(original_vpk_files,map_path)
-    down_file = Path(map_path,name)
+    original_vpk_files = get_vpk(original_vpk_files,map_paths)
+    down_file = Path(map_paths,name)
     if await get_file(url,down_file) == None:
         return None
     sleep(1)
@@ -24,33 +26,54 @@ async def updown_l4d2_vpk(map_path,name,url):
     
     sleep(1)
     extracted_vpk_files = []
-    extracted_vpk_files = get_vpk(extracted_vpk_files,map_path)
-    logger.info(extracted_vpk_files)
+    extracted_vpk_files = get_vpk(extracted_vpk_files,map_paths)
     # 获取新增vpk文件的list
     vpk_files = list(set(extracted_vpk_files) - set(original_vpk_files))
     return  vpk_files
 
 def open_packet(name:str,down_file:Path):
     """解压压缩包"""
-    zip_dir = os.path.dirname(down_file)
+    down_path = os.path.dirname(down_file)
     logger.info('文件名为：' + name)
-    if name.endswith('.zip'):
-        mes = 'zip文件已下载,正在解压'
-        with support_gbk(ZipFile(down_file, 'r')) as z:
-            z.extractall(zip_dir)
-        os.remove(down_file)
-    elif name.endswith('.7z'):
-        mes ='7z文件已下载,正在解压'
-        with support_gbk(SevenZipFile(down_file, 'r')) as z:
-            z.extractall(zip_dir)
-        os.remove(down_file)
-    elif name.endswith('rar'):
-        mes = 'rar文件已下载,正在解压'
-        with support_gbk(RarFile(down_file,'r')) as z:
-            z.extractall(zip_dir)
-        os.remove(down_file)
-    elif name.endswith('.vpk'):
-        mes ='vpk文件已下载'
+    logger.info(f'系统为{systems}')
+    if systems == 'win':
+        if name.endswith('.zip'):
+            mes = 'zip文件已下载,正在解压'
+            with ZipFile(down_file, 'r') as z:
+                z.extractall(down_path)
+            os.remove(down_file)
+        elif name.endswith('.7z'):
+            mes ='7z文件已下载,正在解压'
+            Archive(down_file).extractall(down_path)
+            # with SevenZipFile(down_file, 'r') as z:
+            #     z.extractall(down_path)
+            os.remove(down_file)
+        elif name.endswith('rar'):
+            mes = 'rar文件已下载,正在解压'
+            with RarFile(down_file,'r') as z:
+                z.extractall(down_path)
+            os.remove(down_file)
+        elif name.endswith('.vpk'):
+            mes ='vpk文件已下载'
+    else:
+        if name.endswith('.zip'):
+            mes = 'zip文件已下载,正在解压'
+            with support_gbk(ZipFile(down_file, 'r')) as z:
+                z.extractall(down_path)
+            os.remove(down_file)
+        elif name.endswith('.7z'):
+            mes ='7z文件已下载,正在解压'
+            Archive(down_file).extractall(down_path)
+            # with SevenZipFile(down_file, 'r') as z:
+            #     z.extractall(down_path)
+            os.remove(down_file)
+        elif name.endswith('rar'):
+            mes = 'rar文件已下载,正在解压'
+            with rarfile.RarFile(down_file,'r') as z:
+                z.extractall(down_path)
+            os.remove(down_file)
+        else:
+            mes ='vpk文件已下载'        
     return mes
 
 def support_gbk(zip_file):
