b59200a685102fb9230cefa47059c8b859d75fbd
Agnes Digital <Z735803792@163.com>
2023-06-21 11:30:23 +0800

    修改错误的元插件位置，以及依赖的依赖版本错误

--- __init__.py
+++ __init__.py
@@ -1,23 +1,6 @@
 from pathlib import Path
 from nonebot import require, load_plugins
-from nonebot.plugin import PluginMetadata
 
-
-__version__ = "0.5.6"
-__plugin_meta__ = PluginMetadata(
-    name="求生之路小助手",
-    description='群内对有关求生之路的查询和操作',
-    usage="""
-    查询：【关键词】([序号])
-    """,
-    type="application",
-    homepage="https://github.com/Agnes4m/nonebot_plugin_l4d2_server",
-    supported_adapters={"~onebot.v11"},
-    extra={
-        "version": __version__,
-        "author": "Agnes4m <Z735803792@163.com>",
-    },
-)
 
 dir_ = Path(__file__).parent
 require('nonebot_plugin_apscheduler')
--- nonebot_plugin_l4d2_server/__init__.py
+++ nonebot_plugin_l4d2_server/__init__.py
@@ -36,10 +36,26 @@ from .l4d2_file import updown_l4d2_vpk,all_zip_to_one
 from .l4d2_file.input_json import *
 from .l4d2_utils.txt_to_img import mode_txt_to_img
 scheduler = require("nonebot_plugin_apscheduler").scheduler
+from nonebot.plugin import PluginMetadata
 
 driver = get_driver()
 
 
+__version__ = "0.5.6"
+__plugin_meta__ = PluginMetadata(
+    name="求生之路小助手",
+    description='群内对有关求生之路的查询和操作',
+    usage="""
+    查询：【关键词】([序号])
+    """,
+    type="application",
+    homepage="https://github.com/Agnes4m/nonebot_plugin_l4d2_server",
+    supported_adapters={"~onebot.v11"},
+    extra={
+        "version": __version__,
+        "author": "Agnes4m <Z735803792@163.com>",
+    },
+)
 
 
 """相当于启动就检查数据库"""
